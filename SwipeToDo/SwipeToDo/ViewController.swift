c51a353f4a1178fb31f36ed3f621651b93ef7288
efreestone <efreestone@fullsail.edu>
2016-01-30 19:59:43 -0700

    Removed unneeded comment

--- SwipeToDo/SwipeToDo/ViewController.swift
+++ SwipeToDo/SwipeToDo/ViewController.swift
@@ -19,8 +19,6 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    //Edit to create and commit working branch 
 
 
 }
